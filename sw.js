4021b466f7f3e9af2bf1bc4ca171ce0d9afd39f6
TheMaffinStaffer98 <120679269+TheMuffinStuffer98@users.noreply.github.com>
2026-09-16 15:29:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker: macht die App offline nutzbar und speichert die OCR-Dateien nach dem ersten Laden. */
-const VERSION = 'assistent-v5';
+const VERSION = 'assistent-v7';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-180.png'];
 const RUNTIME_HOSTS = ['cdn.jsdelivr.net', 'tessdata.projectnaptha.com', 'fonts.googleapis.com', 'fonts.gstatic.com'];
 
